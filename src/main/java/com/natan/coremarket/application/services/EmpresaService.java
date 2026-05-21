28daca9452068cb6971b03c46efc6c205c39671d
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-21 08:13:57 -0300

    feat: implementar camada de serviço da empresa

---
+++ src/main/java/com/natan/coremarket/application/services/EmpresaService.java
@@ -0,0 +1,26 @@
+package com.natan.coremarket.application.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.natan.coremarket.domain.entities.Empresa;
+import com.natan.coremarket.infrastructure.repositories.EmpresaRepository;
+
+@Service
+public class EmpresaService {
+    
+    private final EmpresaRepository empresaRepository;
+
+    public EmpresaService(EmpresaRepository empresaRepository) {
+        this.empresaRepository = empresaRepository;
+    }
+
+    public Empresa salvar(Empresa empresa) {
+        return empresaRepository.save(empresa);
+    }
+
+    public List<Empresa> listarTodas() {
+        return empresaRepository.findAll();
+    }
+}
