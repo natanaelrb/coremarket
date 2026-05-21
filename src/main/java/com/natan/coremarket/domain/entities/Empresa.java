42a2d1a2bdbcacf7ed9027221b7533c93564b227
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-21 08:13:23 -0300

    feat: criar entidade empresa

--- src/main/java/com/natan/coremarket/domain/entities/Empresa.java
+++ src/main/java/com/natan/coremarket/domain/entities/Empresa.java
@@ -1,5 +1,53 @@
 package com.natan.coremarket.domain.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empresa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = false)
+    private String telefone;
+
+    @Column(unique = true, nullable = false)
+    private String cnpj;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
     
 }
