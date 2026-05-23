6dc68aec3f8c3943da2f330370ffdc9e74ae8a3f
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-23 10:45:27 -0300

    feat: criar entidade cliente

--- src/main/java/com/natan/coremarket/domain/entities/Cliente.java
+++ src/main/java/com/natan/coremarket/domain/entities/Cliente.java
@@ -1,5 +1,61 @@
 package com.natan.coremarket.domain.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(length = 500)
+    private String observacao;
+
+    @Column(name = "telefone", nullable = false)
+    private String telefone;
+
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "empresa_id", nullable = false)
+    private Empresa empresa;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
     
 }
