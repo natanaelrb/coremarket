13d451a27c0e57c24ca42ff4e6f646ca3a3827c8
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-23 10:46:32 -0300

    feat: criar endpoints REST do cliente

--- src/main/java/com/natan/coremarket/infrastructure/controllers/ClienteController.java
+++ src/main/java/com/natan/coremarket/infrastructure/controllers/ClienteController.java
@@ -1,5 +1,59 @@
 package com.natan.coremarket.infrastructure.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.natan.coremarket.application.services.ClienteService;
+import com.natan.coremarket.domain.entities.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
     
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return clienteService.salvar(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listarTodas() {
+        return clienteService.listarTodos();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return clienteService.buscarPorId(id)
+            .map(cliente -> ResponseEntity.ok().body(cliente))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteService.atualizar(id, cliente)
+            .map(clienteAtualizado -> ResponseEntity.ok().body(clienteAtualizado))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        clienteService.deletar(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
