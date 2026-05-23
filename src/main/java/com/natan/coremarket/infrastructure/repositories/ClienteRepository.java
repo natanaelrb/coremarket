c814aab83cd055157ff9d803c04969ae3bf7628c
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-23 10:45:53 -0300

    feat: criar repository do cliente

--- src/main/java/com/natan/coremarket/infrastructure/repositories/ClienteRepository.java
+++ src/main/java/com/natan/coremarket/infrastructure/repositories/ClienteRepository.java
@@ -1,5 +1,15 @@
 package com.natan.coremarket.infrastructure.repositories;
 
-public class ClienteRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.natan.coremarket.domain.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByEmpresaId(Long empresaId);
     
 }
