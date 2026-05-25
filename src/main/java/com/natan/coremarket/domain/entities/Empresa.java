23c9d01c8931a65fd3e228a60de5cbc0fae86dfc
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-25 10:24:30 -0300

    refactor: Remover imports inutilizáveis.

--- src/main/java/com/natan/coremarket/domain/entities/Empresa.java
+++ src/main/java/com/natan/coremarket/domain/entities/Empresa.java
@@ -1,16 +1,13 @@
 package com.natan.coremarket.domain.entities;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
