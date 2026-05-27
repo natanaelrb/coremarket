0d80a0018c6cee98cb9ed3e79630e522054ca6a8
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-27 10:37:12 -0300

    feat: adicionar manipulador de exceção global para erros de validação

---
+++ src/main/java/com/natan/coremarket/infrastructure/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.natan.coremarket.infrastructure.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            erros.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return erros;
+    }
+}
+
